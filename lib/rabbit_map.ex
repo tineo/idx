8b22deb4925ac91544264278137d54b29db80acc
Cesar Gutierrez <tineo@dgt.dgt-domain>
2018-02-26 17:31:51 -0500

    memory

--- lib/rabbit_map.ex
+++ lib/rabbit_map.ex
@@ -1,24 +1,27 @@
 defmodule Dgtidx.RabbitMap do
   @moduledoc false
 
+  @queue  Application.get_env(:dgtidx, :queue)
+  @exchange  Application.get_env(:dgtidx, :exchange)
+
   def get_amqp_channels() do
-    queue_name = "to_process_w2"
-    queue_name2 = "to_process_w2_fn"
-    queue_name3 = "to_process_w2_fn_3"
-    queue_name4 = "to_process_w2_fn_4"
-    exchange_name = "ex_w2"
+    queue_name = @queue
+    #queue_name2 = "to_process_w2_fn"
+    #queue_name3 = "to_process_w2_fn_3"
+    #queue_name4 = "to_process_w2_fn_4"
+    exchange_name = @exchange
 
     config  = Application.get_env(:dgtidx, __MODULE__)
 
     {:ok, connection} = AMQP.Connection.open(config[:url])
 
-    {:ok, connection2} = AMQP.Connection.open(config[:url])
-    {:ok, connection3} = AMQP.Connection.open(config[:url])
+    #{:ok, connection2} = AMQP.Connection.open(config[:url])
+    #{:ok, connection3} = AMQP.Connection.open(config[:url])
 
     {:ok, channel} = AMQP.Channel.open(connection)
 
-    {:ok, channel2} = AMQP.Channel.open(connection2)
-    {:ok, channel3} = AMQP.Channel.open(connection3)
+    #{:ok, channel2} = AMQP.Channel.open(connection2)
+    #{:ok, channel3} = AMQP.Channel.open(connection3)
 
     AMQP.Queue.declare(channel, queue_name, durable: true)
     AMQP.Exchange.direct(channel, exchange_name, durable: true)
@@ -26,17 +29,17 @@ defmodule Dgtidx.RabbitMap do
     #AMQP.Basic.qos(channel, prefetch_count: 1)
     AMQP.Basic.consume(channel, queue_name, nil, no_ack: false)
 
-    AMQP.Queue.declare(channel, queue_name2, durable: true)
-    AMQP.Queue.bind(channel, queue_name2, exchange_name)
-    AMQP.Basic.consume(channel, queue_name2, nil, no_ack: false)
+    #AMQP.Queue.declare(channel, queue_name2, durable: true)
+    #AMQP.Queue.bind(channel, queue_name2, exchange_name)
+    #AMQP.Basic.consume(channel, queue_name2, nil, no_ack: false)
 
-    AMQP.Queue.declare(channel, queue_name3, durable: true)
-    AMQP.Queue.bind(channel, queue_name3, exchange_name)
-    AMQP.Basic.consume(channel, queue_name3, nil, no_ack: false)
+    #AMQP.Queue.declare(channel, queue_name3, durable: true)
+    #AMQP.Queue.bind(channel, queue_name3, exchange_name)
+    #AMQP.Basic.consume(channel, queue_name3, nil, no_ack: false)
 
-    AMQP.Queue.declare(channel, queue_name4, durable: true)
-    AMQP.Queue.bind(channel, queue_name4, exchange_name)
-    AMQP.Basic.consume(channel, queue_name4, nil, no_ack: false)
+    #AMQP.Queue.declare(channel, queue_name4, durable: true)
+    #AMQP.Queue.bind(channel, queue_name4, exchange_name)
+    #AMQP.Basic.consume(channel, queue_name4, nil, no_ack: false)
 
     [
       channel#,
